61ed764ea553fba5807d1e9ddff1bf9d9e1f21e4
Rohit Raj <rajrohit7388@outlook.com>
2026-02-06 22:20:08 +0530

    Add files via upload

---
+++ lastoccurance.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class lastoccurance
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = {1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 11};
+        int target = 5;
+
+        int lo = 0;
+        int hi = arr.length - 1;
+        int mid = 0;
+        int flag = 0;
+        int idx = -1;
+
+        while(lo <= hi)
+        {
+            mid = (lo + hi) / 2;
+            if(arr[mid] > target)
+                hi = mid - 1;
+            else if(arr[mid] < target)
+                lo = mid + 1;
+            else if(arr[mid] == target)
+            {
+                flag = 1;
+                idx = mid;
+                lo = mid + 1;
+            }
+        }
+        if(flag == 1)
+        {
+            System.out.println("found last at : "+idx);
+        }
+    }
+}
